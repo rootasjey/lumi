82509301ef7cba583212fba17320398b5f5805ae
rootasjey <jeremiecorpinot@outlook.com>
2020-09-26 11:42:05 +0200

    ➕ Add LightCard component

---
+++ lib/components/light_card.dart
@@ -0,0 +1,243 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:hue_dart/hue_dart.dart' hide Timer;
+import 'package:lumi/screens/home/light_page.dart';
+import 'package:lumi/state/colors.dart';
+import 'package:lumi/state/user_state.dart';
+import 'package:supercharged/supercharged.dart';
+
+class LightCard extends StatefulWidget {
+  final Light light;
+
+  LightCard({@required this.light});
+
+  @override
+  _LightCardState createState() => _LightCardState();
+}
+
+class _LightCardState extends State<LightCard> {
+  bool isLoading = false;
+  Color accentColor;
+
+  double brightness;
+  Light light;
+
+  Timer updateBrightnessTimer;
+  Timer timerUpdate;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      light = widget.light;
+      brightness = light.state.brightness.toDouble();
+      accentColor = stateColors.primary;
+    });
+
+    fetch();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (context) {
+        return Hero(
+          tag: light.id,
+          child: Container(
+            width: 240.0,
+            height: 240.0,
+            child: Card(
+              elevation: 6.0,
+              child: InkWell(
+                onTap: () => onTapLightCard(light),
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              bottom: 8.0,
+                            ),
+                            child: Text(
+                              light.name,
+                              style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+
+                          brightnessSlider(light),
+                        ],
+                      ),
+                    ),
+
+                    Positioned(
+                      top: 20.0,
+                      left: 20.0,
+                      child: IconButton(
+                        tooltip: 'Turn ${light.state.on ? 'off' : 'on'}',
+                        onPressed: () async {
+                          final isOn = light.state.on;
+
+                          LightState state = LightState(
+                            (l) => l..on = !isOn
+                          );
+
+                          await userState.bridge.updateLightState(
+                            light.rebuild(
+                              (l) => l..state = state.toBuilder()
+                            )
+                          );
+
+                          fetch();
+                        },
+                        icon: Icon(
+                          Icons.lightbulb_outline,
+                          size: 30.0,
+                          color: light.state.on
+                            ? accentColor
+                            : stateColors.foreground.withOpacity(0.6)
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      }
+    );
+  }
+
+  Widget brightnessSlider(Light light) {
+    return Slider(
+      value: brightness,
+      min: 0,
+      max: 254,
+      activeColor: light.state.on
+        ? accentColor
+        : stateColors.foreground.withOpacity(0.4),
+
+      inactiveColor: stateColors.foreground.withOpacity(0.4),
+      label: brightness.round().toString(),
+
+      onChanged: (double value) async {
+        setState(() {
+          brightness = value;
+        });
+
+        if (updateBrightnessTimer != null) {
+          updateBrightnessTimer.cancel();
+        }
+
+        updateBrightnessTimer = Timer(
+          250.milliseconds,
+          () async {
+            LightState state = LightState(
+              (l) => l..brightness = value.toInt()
+            );
+
+            if (!light.state.on) {
+              state = LightState(
+                (l) => l
+                  ..on = true
+                  ..brightness = value.toInt()
+              );
+            }
+
+            await userState.bridge.updateLightState(
+              light.rebuild(
+                (l) => l..state = state.toBuilder()
+              )
+            );
+
+            fetch();
+          }
+        );
+      },
+    );
+  }
+
+  void onTapLightCard(Light light) async {
+    await Navigator.of(context).push(
+      MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (context) {
+          return Scaffold(
+            body: Hero(
+              tag: light.id,
+              child: Center(
+                child: Container(
+                  width: 800,
+                  padding: const EdgeInsets.all(80.0),
+                  child: Card(
+                    elevation: 8.0,
+                    child: LightPage(
+                      id: light.id.toString(),
+                      light: light,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      )
+    );
+
+    fetch();
+  }
+
+  /// Fetch a single light's data.
+  void fetch() async {
+    if (!light.state.on) {
+      return;
+    }
+
+    if (isLoading && timerUpdate != null) {
+      timerUpdate.cancel();
+    }
+
+    isLoading = true;
+
+    timerUpdate = Timer(
+      150.milliseconds,
+      () async {
+        try {
+          light = await userState.bridge.light(light.id);
+
+          if (!mounted) {
+            return;
+          }
+
+          final hsl = HSLColor.fromAHSL(
+            1.0,
+            light.state.hue / 65535 * 360,
+            light.state.saturation / 255,
+            light.state.brightness / 255,
+          );
+
+          setState(() {
+            isLoading   = false;
+            accentColor = hsl.toColor();
+            brightness  = light.state.brightness.toDouble();
+          });
+
+        } catch (error) {
+          debugPrint(error.toString());
+          isLoading = false;
+          setState(() {});
+        }
+      }
+    );
+  }
+}
--- lib/screens/home/lights.dart
+++ lib/screens/home/lights.dart
@@ -1,14 +1,11 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:hue_dart/hue_dart.dart' hide Timer;
 import 'package:lumi/components/error_view.dart';
+import 'package:lumi/components/light_card.dart';
 import 'package:lumi/components/loading_view.dart';
-import 'package:lumi/screens/home/light_page.dart';
-import 'package:lumi/state/colors.dart';
 import 'package:lumi/state/user_state.dart';
-import 'package:supercharged/supercharged.dart';
 
 class Lights extends StatefulWidget {
   @override
@@ -58,136 +55,10 @@ class _LightsState extends State<Lights> {
       delegate: SliverChildBuilderDelegate(
         (context, index) {
           final light = lights[index];
-          return card(light);
+          return LightCard(light: light);
         },
         childCount: lights.length,
       ),
-    );
-  }
-
-  Widget card(Light light) {
-    return Observer(
-      builder: (context) {
-        return Hero(
-          tag: light.id,
-          child: Container(
-            width: 240.0,
-            height: 240.0,
-            child: Card(
-              elevation: 6.0,
-              child: InkWell(
-                onTap: () => onTapLightCard(light),
-                child: Stack(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(30.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                              bottom: 8.0,
-                            ),
-                            child: Text(
-                              light.name,
-                              style: TextStyle(
-                                fontSize: 24.0,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ),
-
-                          brightnessSlider(light),
-                        ],
-                      ),
-                    ),
-
-                    Positioned(
-                      top: 20.0,
-                      left: 20.0,
-                      child: IconButton(
-                        tooltip: 'Turn ${light.state.on ? 'off' : 'on'}',
-                        onPressed: () async {
-                          final isOn = light.state.on;
-
-                          LightState state = LightState(
-                            (l) => l..on = !isOn
-                          );
-
-                          await userState.bridge.updateLightState(
-                            light.rebuild(
-                              (l) => l..state = state.toBuilder()
-                            )
-                          );
-
-                          fetch();
-                        },
-                        icon: Icon(
-                          Icons.lightbulb_outline,
-                          size: 30.0,
-                          color: light.state.on
-                            ? stateColors.primary
-                            : stateColors.foreground.withOpacity(0.6)
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        );
-      }
-    );
-  }
-
-  Widget brightnessSlider(Light light) {
-    return Slider(
-      value: sliderValues[light.id],
-      min: 0,
-      max: 254,
-      activeColor: light.state.on
-        ? stateColors.primary
-        : stateColors.foreground.withOpacity(0.4),
-
-      inactiveColor: stateColors.foreground.withOpacity(0.4),
-      label: sliderValues[light.id].round().toString(),
-
-      onChanged: (double value) async {
-        setState(() {
-          sliderValues[light.id] = value;
-        });
-
-        if (updateBrightnessTimer != null) {
-          updateBrightnessTimer.cancel();
-        }
-
-        updateBrightnessTimer = Timer(
-          250.milliseconds,
-          () async {
-            LightState state = LightState(
-              (l) => l..brightness = value.toInt()
-            );
-
-            if (!light.state.on) {
-              state = LightState(
-                (l) => l
-                  ..on = true
-                  ..brightness = value.toInt()
-              );
-            }
-
-            await userState.bridge.updateLightState(
-              light.rebuild(
-                (l) => l..state = state.toBuilder()
-              )
-            );
-
-            fetch();
-          }
-        );
-      },
     );
   }
 
@@ -219,35 +90,5 @@ class _LightsState extends State<Lights> {
 
       debugPrint(error.toString());
     }
-  }
-
-  void onTapLightCard(Light light) async {
-    await Navigator.of(context).push(
-      MaterialPageRoute(
-        fullscreenDialog: true,
-        builder: (context) {
-          return Scaffold(
-            body: Hero(
-              tag: light.id,
-              child: Center(
-                child: Container(
-                  width: 800,
-                  padding: const EdgeInsets.all(80.0),
-                  child: Card(
-                    elevation: 8.0,
-                    child: LightPage(
-                      id: light.id.toString(),
-                      light: light,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          );
-        },
-      )
-    );
-
-    fetch();
   }
 }
