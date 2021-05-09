1078eadfb3fa6c4dea9d3543bc1fbb444b5e5fcf
rootasjey <rootasjey@users.noreply.github.com>
2021-05-10 00:59:50 +0200

    🚀  Update app version to [0.40.0]

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,10 +4,10 @@ const String KEY_USER_NAME = 'username';
 const String KEY_SETTINGS = 'settings';
 
 class Constants {
-  static const appVersion = '0.4.0';
+  static const appVersion = '0.40.0';
   static const appBuildNumber = 1;
   static const cardElevation = 2.0;
   static const maxMobileHeight = 400.0;
   static const maxMobileWidth = 700.0;
-  static const appGithubUrl = 'https://github.com/rootasjey/rootasjey.dev';
+  static const appGithubUrl = 'https://github.com/rootasjey/lumi';
 }
