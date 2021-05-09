5324628baa0e1999a443d7d9f0cfce7c097f1e23
rootasjey <rootasjey@users.noreply.github.com>
2021-05-09 22:56:20 +0200

    ☝️ Update [ColorCard] component w/ better hover

--- lib/components/color_card.dart
+++ lib/components/color_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supercharged/supercharged.dart';
 
 class ColorCard extends StatefulWidget {
   final Color color;
@@ -13,26 +14,52 @@ class ColorCard extends StatefulWidget {
   _ColorCardState createState() => _ColorCardState();
 }
 
-class _ColorCardState extends State<ColorCard> {
-  double elevation = 4.0;
+class _ColorCardState extends State<ColorCard> with TickerProviderStateMixin {
+  Animation<double> _scaleAnimation;
+  AnimationController _scaleAnimationController;
+
+  double _elevation = 4.0;
+
+  @override
+  initState() {
+    super.initState();
+
+    _scaleAnimationController = AnimationController(
+      lowerBound: 0.8,
+      upperBound: 1.0,
+      duration: 250.milliseconds,
+      vsync: this,
+    );
+
+    _scaleAnimation = CurvedAnimation(
+      parent: _scaleAnimationController,
+      curve: Curves.fastOutSlowIn,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 100.0,
-      height: 100.0,
-      child: Card(
-        elevation: elevation,
-        color: widget.color,
-        child: InkWell(
-          onTap: widget.onTap,
-          onHover: (isHover) {
-            setState(() {
-              elevation = isHover
-                ? 8.0
-                : 4.0;
-            });
-          },
+    return ScaleTransition(
+      scale: _scaleAnimation,
+      child: SizedBox(
+        width: 100.0,
+        height: 100.0,
+        child: Card(
+          elevation: _elevation,
+          color: widget.color,
+          child: InkWell(
+            onTap: widget.onTap,
+            onHover: (isHover) {
+              if (isHover) {
+                _scaleAnimationController.forward();
+                setState(() => _elevation = 8.0);
+                return;
+              }
+
+              _scaleAnimationController.reverse();
+              setState(() => _elevation = 4.0);
+            },
+          ),
         ),
       ),
     );
